cd904115b51f467f50caecadb053c9d4dbf28fe1
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-12-01 10:32:51 +0000

    updates

--- sleep_in.py
+++ sleep_in.py
@@ -1,5 +1,5 @@
 # The parameter weekday is True if it is a weekday, and the parameter
-# vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation.
+# vacation is True if we are on vacation. We sleep in if it is not a weekday, or we're on vacation.
 # Return True if we sleep in.
 #
 # sleep_in(False, False) → True
@@ -10,5 +10,4 @@ def sleep_in(weekday, vacation):
     if not weekday or vacation:
         return True
     return False
-
 
